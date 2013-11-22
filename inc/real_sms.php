32efe74d1b5a7e1c3262884d284a67e50e8bc277
Jason El-Massih <jel-massih@hotmail.com>
2013-11-21 23:12:18 -0500

    Cleaned up Unnecessary Files

--- inc/real_sms.php
+++ inc/real_sms.php
@@ -1,9 +1,9 @@
 <?php
 	$body2 = strtolower($_POST['Body']);
 	if (strpos($body2,'woo') !== false) {
-		$ch = curl_init("http://162.243.61.109:8080?type=vote&v=1");
+		$ch = curl_init("http://192.241.169.33:8080?type=vote&v=1");
 	}else if (strpos($body2,'boo') !== false) {
-		$ch = curl_init("http://162.243.61.109:8080?type=vote&v=-1");
+		$ch = curl_init("http://192.241.169.33:8080?type=vote&v=-1");
 	}
 
     curl_exec($ch);
--- inc/sms.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include_once('../../APISearch/inc/twilioHandler.php');
-
-	$body = $_POST['Body'];
-	
-?>
